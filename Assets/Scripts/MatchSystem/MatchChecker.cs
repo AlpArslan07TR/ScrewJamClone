2b5e0dfe942bd8ee38c4e49573918514b290cbde
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-10-25 20:16:15 +0300

    MatchChecker Updated
    1-Second match error fixed

--- Assets/Scripts/MatchSystem/MatchChecker.cs
+++ Assets/Scripts/MatchSystem/MatchChecker.cs
@@ -5,13 +5,32 @@ using DG.Tweening;
 public class MatchChecker : MonoBehaviour
 {
     public List<Transform> screwsInHoldPlace = new List<Transform>(); 
+    public Transform holdPlace; 
+
     
     public void CheckForMatch()
     {
+       
+        List<Transform> currentScrews = new List<Transform>();
+
+        
+        foreach (Transform holdPosition in holdPlace)
+        {
+            for (int i = 0; i < holdPosition.childCount; i++)
+            {
+                Transform childScrew = holdPosition.GetChild(i);
+                if (childScrew != null) 
+                {
+                    currentScrews.Add(childScrew); 
+                }
+            }
+        }
+
         
         Dictionary<string, List<Transform>> screwGroups = new Dictionary<string, List<Transform>>();
 
-        foreach (var screw in screwsInHoldPlace)
+        
+        foreach (var screw in currentScrews)
         {
             string screwTag = screw.tag; 
             if (!screwGroups.ContainsKey(screwTag))
@@ -21,12 +40,13 @@ public class MatchChecker : MonoBehaviour
             screwGroups[screwTag].Add(screw);
         }
 
-        
+       
         foreach (var group in screwGroups)
         {
+            Debug.Log($"Grup: {group.Key}, Vida Sayýsý: {group.Value.Count}"); 
             if (group.Value.Count >= 3) 
             {
-                
+                // Sadece vidalarý yok et
                 foreach (var screw in group.Value)
                 {
                     screw.DOScale(Vector3.zero, 0.5f).OnComplete(() =>
@@ -39,5 +59,9 @@ public class MatchChecker : MonoBehaviour
                 return; 
             }
         }
+
+        
+        screwsInHoldPlace.Clear();
+        screwsInHoldPlace.AddRange(currentScrews);
     }
 }
